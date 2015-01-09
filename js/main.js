6194fafe35a1813e36b8f976423be80abb7a4dca
swapanju38 <niswapan@gmail.com>
2015-01-08 20:42:51 -0800

    Initial Commit
    My Project PSD to HTML

---
+++ js/main.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function($){
+	jQuery('#nav').slicknav();
+
+    $("#owl-demo").owlCarousel({
+ 
+	autoPlay: 3000, //Set AutoPlay to 3 seconds
+	 
+	items : 4,
+	itemsDesktop : [1199,3],
+	itemsDesktopSmall : [979,3]
+	 
+	});
+
+});
+
